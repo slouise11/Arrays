2883f16a3a6472a97267cf5842cdba6417e38e9c
Sarah <Sarah@DESKTOP-M75431O>
2024-10-13 16:57:20 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Arrays";
+
+            string[] cars = new string[3] { "BMW", "Ford", "Opel" };
+
+            int[,] coords = new int[2, 3] { { 1, 2, 3 } , { 4, 5, 6 } };
+
+            Console.WriteLine("Second Car: " + cars[1]);
+            Console.WriteLine("X1,Y1: " + coords[0, 0]);
+            Console.WriteLine("X2,Y3: " + coords[1,2]); ;
+            Console.ReadKey();
+        }
+    }
+}
